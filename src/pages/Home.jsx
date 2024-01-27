58852258a910c5ffd4d8d9ade2750f8d6d845d90
Aditya <holaaditya123@gmail.com>
2024-01-27 23:41:42 +0530

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import Plane from "../models/Plane";
 }
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -63,13 +64,14 @@ const Home = () => {
             intensity={1}
           />
           <Bird />
-          <Sky />
+          <Sky isRotating={isRotating} />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
             isRotating={isRotating}
             setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
           <Plane
             planeScale={planeScale}
