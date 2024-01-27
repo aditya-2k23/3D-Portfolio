50b9812ad4ed1d6a9834c50e2f378bb79c32629b
Aditya <holaaditya123@gmail.com>
2024-01-27 23:26:45 +0530

    Island Section Done

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -153,6 +153,7 @@ export function Island({
   });
 
   return (
+    // {Island 3D model from: https://sketchfab.com/3d-models/foxs-islands-163b68e09fcc47618450150be7785907}
     <a.group ref={islandRef} {...props}>
       <mesh
         geometry={nodes.polySurface944_tree_body_0.geometry}
